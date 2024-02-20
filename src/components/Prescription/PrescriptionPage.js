3aacb3c283e3a4891372668b76bf572ca0e6d294
Rubello <46662591+Rubello21@users.noreply.github.com>
2024-02-20 15:00:56 -0500

    adding home button in prescription page

--- src/components/Prescription/PrescriptionPage.js
+++ src/components/Prescription/PrescriptionPage.js
@@ -11,163 +11,167 @@ function PrescriptionPage() {
 
         <div className='right-panel'><RightPanel currentPage="prescriptions" /></div>  
 
-              {/* Active Prescriptions */}
-      <h2 id="active">Active Prescriptions</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date prescribed</th>
-            <th>Expiring Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Rows for active prescriptions */}
-          <tr>
-            <td>Prescription 1</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          {/* Add more rows */}
-        </tbody>
-      </table>
+        {/* Home Button */}
+        <div className="home-button">
+          <button className="home-btn">Home</button>
+        </div>
 
-      {/* To Renew Prescriptions */}
-      <h2 id="to-renew">Prescriptions To Renew</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date prescribed</th>
-            <th>Expiring Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Rows for prescriptions to renew */}
-          <tr>
-            <td>Prescription 1</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
+        {/* Active Prescriptions */}
+        <h2 id="active">Active Prescriptions</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date prescribed</th>
+              <th>Expiring Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Rows for active prescriptions */}
+            <tr>
+              <td>Prescription 1</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            {/* Add more rows */}
+          </tbody>
+        </table>
 
-          <tr>
-            <td>Prescription 3</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 4</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 5</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 6</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 7</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 8</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 9</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 10</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 11</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 12</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 13</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 14</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 15</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 16</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 17</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          {/* Add more rows */}
-        </tbody>
-      </table>
+        {/* To Renew Prescriptions */}
+        <h2 id="to-renew">Prescriptions To Renew</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date prescribed</th>
+              <th>Expiring Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Rows for prescriptions to renew */}
+            <tr>
+              <td>Prescription 1</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
 
-      {/* Expired Prescriptions */}
-      <h2 id="expired">Expired Prescriptions</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Name</th>
-            <th>Date prescribed</th>
-            <th>Expiring Date</th>
-          </tr>
-        </thead>
-        <tbody>
-          {/* Rows for expired prescriptions */}
-          <tr>
-            <td>Prescription 1</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          <tr>
-            <td>Prescription 2</td>
-            <td>YYYY-MM-DD</td>
-            <td>YYYY-MM-DD</td>
-          </tr>
-          {/* Add more rows */}
-        </tbody>
-      </table>
+            <tr>
+              <td>Prescription 3</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 4</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 5</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 6</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 7</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 8</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 9</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 10</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 11</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 12</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 13</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 14</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 15</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 16</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 17</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            {/* Add more rows */}
+          </tbody>
+        </table>
+
+        {/* Expired Prescriptions */}
+        <h2 id="expired">Expired Prescriptions</h2>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Date prescribed</th>
+              <th>Expiring Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* Rows for expired prescriptions */}
+            <tr>
+              <td>Prescription 1</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            <tr>
+              <td>Prescription 2</td>
+              <td>YYYY-MM-DD</td>
+              <td>YYYY-MM-DD</td>
+            </tr>
+            {/* Add more rows */}
+          </tbody>
+        </table>
         
       </div>
       <div className="dropdown-container">
-      <div className="dropdown">
-        <button className="dropbtn">Languages</button>
-        <div className="dropdown-content">
-          <button>English</button>
-          <button>Spanish</button>
-          <button>French</button>
+        <div className="dropdown">
+          <button className="dropbtn">Languages</button>
+          <div className="dropdown-content">
+            <button>English</button>
+            <button>Spanish</button>
+            <button>French</button>
+          </div>
         </div>
       </div>
     </div>
-    </div>
-    
   );
 }
 
